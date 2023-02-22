d7a24c05a8d0df461423ad2ddbde40fda989c944
zhaojiahua <53359717+zhaojiahua@users.noreply.github.com>
2023-02-22 17:09:07 +0800

    Add files via upload

---
+++ shaders/vertexshader.c
@@ -0,0 +1,19 @@
+#version 330 core
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aNormal;
+layout(location = 2) in vec2 aUV;
+
+out vec2 fragUV;
+out vec3 fragNr;
+out vec3 fragPos;
+
+uniform mat4 p_matrix;
+uniform mat4 v_matrix;
+
+void main()
+{
+	fragPos = aPos;
+	gl_Position = p_matrix * v_matrix * vec4(aPos, 1.0);
+	fragUV = aUV;
+	fragNr = aNormal;
+}
